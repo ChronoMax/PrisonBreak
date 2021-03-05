f776532c7cf1cab74db292460139ba5732416cca
MaxStruijs <maxstruys32@gmail.com>
2021-03-05 15:21:38 +0100

    Added Rest Client assets

---
+++ Assets/Scripts/API/Google/GoogleAuthHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GoogleAuthHandler
+{
+    public static void GetUserCode()
+    {
+
+    }
+}
